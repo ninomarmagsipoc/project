66d6705f04051d7cb6d77a4d0137c3caf2ea7907
Nino Mar Magsipoc <ninomarmagsipoc@gmail.com>
2026-03-05 00:18:12 +0800

    s

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dashboard',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       initialRoute: '/',
