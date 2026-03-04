33803de53a5f2ba8f52d3bcd7da185f045c943b2
Nino Mar Magsipoc <ninomarmagsipoc@gmail.com>
2026-03-04 23:23:38 +0800

    s

--- lib/service/anime.dart
+++ lib/service/anime.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AnimePage extends StatelessWidget {
+class AnimePage extends StatefulWidget {
   const AnimePage({super.key});
 
+  @override
+  State<AnimePage> createState() => _AnimePage();
+}
+
+class _AnimePage extends State<AnimePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
